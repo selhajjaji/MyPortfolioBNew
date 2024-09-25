8c2cba88035f00a1c666b190a88e3a40e747f879
Sabra el hajjaji <selhajja@my.centennialcollege.ca>
2024-09-25 19:26:26 -0400

    Home and about modified

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,5 +1,5 @@
-import BBG from '../src/assets/BBG.jfif';
 import hire from '../src/assets/hire.jpg';
+import myPhoto from '../src/assets/myPhoto.jpg';
 import { Link } from 'react-router-dom';
 import '../src/index.css'
 
@@ -9,10 +9,12 @@ export default function Home() {
      <section id="intro">
           <div className="introContent">
           <span className="hello">Hello,</span><br/>
-          <span className="introText">I am <span className="introName">Blessing Ajiboye</span> 
-          <img src={BBG} alt="profile" className="bbg" width="750" height="750" align="right" />
-          <br/>Website Designer</span>
-          <p className="introPara">I am a skilled web designer with experience in creating visually appealing user friendly websites.</p>
+          <span className="introText">I am <span className="introName">Sabra Elhajjaji</span> 
+          <img src={myPhoto} alt="profile" className="bbg" width="750" height="750" align="right" />
+          <br/>Backend developer</span>
+          <p className="introPara">Six years of experience in software development with a focus on backend development</p>
+          <p className="introPara">Bilingual competencies in French and English plus the Arabic mother tongue.</p>
+
           <Link><button className="btn"><img src={hire} alt="Hire me" width="100px" height="100px"/></button></Link>
           
           </div>
