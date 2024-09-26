d1855f66dccf90cc8ad930ad9a3d25834d1c8b4d
Sabra el hajjaji <selhajja@my.centennialcollege.ca>
2024-09-25 20:10:08 -0400

    Modified education .jsc and adding related css and

--- client/src/education.jsx
+++ client/src/education.jsx
@@ -1,8 +1,21 @@
+import '../src/education.css'
 export default function Education() {
-     return (
-     <>
-    <p>Education.Qualification</p>
+     return <>
+
+    <section id="works">
+     <h2 className="degrees">DEGREES</h2>
+    <p> Master’s Degree in Applied Computer Science from Mohammed V University,
+    Morocco -Educational credential assessment by World Education Services Ref N° 5275540IMM</p>
+    <p>Bachelor's degree in Mathematics and Computer Science from Mohammed V University,
+ Morocco -Educational credential assessment by World Education Services Ref N° 5275540IMM</p>
+
+ <h2 className="degrees">CERTIFICATIONS</h2>
+    <p>Artificial Intelligence Fundamentals issued by IBM SkillsBuild</p>
+    <p>Certification of “Compétences pour réussir” program at Lacité college -Ottawa</p>
+    <p>Certification of “PAMEC” program (Programme d’accès au marché de l’emploi canadien) at Lacité
+    college - Ottawa </p>
+ </section>
      </>
-     );
+   
      }
     
